3dcf69f335a7d037720806fefdc47e211850e6a6
Jorge Martín <grusite@gmail.com>
2022-07-01 00:54:02 +0200

    add cloudinary to upload imgs

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+        folder: env("CLOUDINARY_FOLDER", "misjuegos"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
